0f60c3a11b62d45cc64a043961a2af038a7d2202
Songsheng Tao <st3107@columbia.edu>
2020-01-08 11:40:42 -0500

    BUG: debug the weight ratio function

--- myscripts/calculation.py
+++ myscripts/calculation.py
@@ -1,45 +1,44 @@
 from diffpy.srreal.scatteringfactortable import SFTXray
 from diffpy.srreal.sfaverage import SFAverage
-from diffpy.structure import loadStructure, Structure
-from typing import Union, List, Tuple, Dict, Iterable, Callable
+from diffpy.structure import loadStructure
+from molmass import Formula
+from typing import Union, List, Tuple, Dict, Iterable
 from pandas import Series
-from uncertainties import ufloat
 
 
-__all__ = ["sfaverage", "molar_fraction"]
+__all__ = ["molar_fraction", "weight_ratio"]
 
 
-def apply_to_multi(func: Callable) -> Callable[[Iterable], List]:
-    """A decorator to make the function 'func(obj)' applicable to multiple objects (like a list or numpy array)"""
-    def _func(objs: Iterable) -> List:
-        lst = []
-        for obj in objs:
-            lst.append(func(obj))
-        return lst
-    return _func
+XTB = SFTXray()
 
 
-def sfaverage(composition: Union[List[Tuple[str, float]], Dict[str, float]] = None,
-              structure: Structure = None, qa=0) -> SFAverage:
-    """Instantiate a x-ray SFAverage from the composition or structure."""
-    xtb = SFTXray()
-    if composition and structure:
-        raise ValueError("Only one of the composition and structure can be used.")
-    elif composition:
-        sfavg = SFAverage.fromComposition(composition, xtb, qa)
-    elif structure:
-
-        sfavg = SFAverage.fromStructure(structure, xtb, qa)
-    else:
-        raise ValueError("Both composition and structure are None")
-    return sfavg
-
-
-@apply_to_multi
-def calc_f1avg(stru_file: str = None) -> float:
+def calc_fs_from_stru_files(*stru_files: str, qa=0) -> List[float]:
     """Calculate the compositional average scattering factor from the structure file."""
-    structure = loadStructure(stru_file)
-    return sfaverage(structure=structure).f1avg
+    fs = []
+    for stru_file in stru_files:
+        stru = loadStructure(stru_file)
+        f = SFAverage.fromStructure(stru, XTB, qa).f1avg
+        fs.append(f)
+    return fs
+
+
+def calc_fs_from_comps(*comps: Dict[str, int], qa=0) -> List[float]:
+    """Calculate the compositional average scattering factor from the compositions."""
+    return [SFAverage.fromComposition(comp, XTB, qa).f1avg for comp in comps]
+
+
+def calc_molar_masses(*comps: Dict[str, int]) -> List[float]:
+    """Calculate the molar masses from the compositions."""
+    def to_string(dct: Dict[str, float]):
+        return ''.join([f'{key}{value}' for key, value in dct.items()])
+
+    masses = []
+    for comp in comps:
+        comp_str = to_string(comp)
+        mass = Formula(comp_str).mass
+        masses.append(mass)
+
+    return masses
 
 
 def molar_fraction(scale: Series, stru_files: Iterable[str] = None) -> Series:
@@ -65,7 +64,43 @@ def molar_fraction(scale: Series, stru_files: Iterable[str] = None) -> Series:
     >>> molar_fraction(scale, stru_files=stru_files)
 
     """
-    f1avg: Series = Series(calc_f1avg(stru_files), index=scale.index)
-    ratio = scale / (f1avg ** 2)
+    f1avg = calc_fs_from_stru_files(*stru_files)
+    sr = Series(f1avg, index=scale.index)
+    ratio = scale / sr.pow(2)
     frac = ratio / ratio.sum()
     return frac
+
+
+def weight_ratio(scale: Series, compositions: List[Dict[str, int]]) -> Series:
+    """
+    Calculate the molar fractions of phases based on its scale factors and compositions or structures.
+
+    Parameters
+    ----------
+    scale : Series
+        A series of scale factors with the name of the phases as index.
+    compositions : Iterable
+        An Iterable of the compositions.
+
+    Returns
+    -------
+    fraction : Series
+        A series of weight ratios with the names of the phases as index.
+
+    Examples
+    --------
+    >>> scale = pd.Series([0.3, 0.4], index=['scale_a', 'scale_b'])
+    >>> composition = [{'atom_A': 1}, {'atom_B': 1}]
+    >>> weight_ratio(scale, compositions)
+
+    """
+    f = calc_fs_from_comps(*compositions)
+    f1avg = Series(f, index=scale.index)
+    m = calc_molar_masses(*compositions)
+    mass = Series(m, index=scale.index)
+
+    ratio = scale / f1avg.pow(2)
+    mol_frac = ratio / ratio.sum()
+    ratio1 = mol_frac * mass
+    fraction = ratio1 / ratio1.sum()
+    return fraction
---
+++ tests/test_calculation.py
@@ -0,0 +1,14 @@
+import myscripts.calculation as calculation
+import pandas as pd
+
+
+def test_weight_ratio():
+    scale = pd.Series([1., 1., 1.])
+    compositions = [{'C': 1}, {'C': 1}, {'C': 1}]
+    weight_ratio = calculation.weight_ratio(scale, compositions)
+    assert isinstance(weight_ratio, pd.Series)
+    assert weight_ratio.to_list() == [1./3, 1./3, 1./3]
+
+
+if __name__ == "__main__":
+    test_weight_ratio()
