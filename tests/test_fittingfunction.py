c9be6b0cb33b4edd8495759ca7a7ef11af5d5463
Songsheng Tao <st3107@columbia.edu>
2020-02-06 14:53:41 -0500

    DEV: developing

--- tests/test_fittingfunction.py
+++ tests/test_fittingfunction.py
@@ -1,5 +1,6 @@
 from myscripts.fittingfunction import *
-from myscripts.fittingclass import GenConfig, ConConfig
+from myscripts.fittingclass import *
+from myscripts.tools import summarize
 from tempfile import TemporaryDirectory
 import os
 import pandas as pd
@@ -9,17 +10,19 @@ def test_fit_ni():
     ni = GenConfig(name="Ni",
                    stru_file="Ni.cif",
                    ncpu=4)
+    f = FunConfig("f", F.sphericalCF, argnames=["r", "psize_Ni"])
 
     config = ConConfig(name="crystal",
                        data_id=0,
                        data_file="Ni.gr",
                        fit_range=(1.5, 10., 0.01),
                        qparams=(0.04, 0.02),
-                       eq="Ni",
-                       phases=ni)
+                       eq="f * Ni",
+                       phases=[ni],
+                       functions=[f])
 
     recipe = make(config)
-
+    cfconstrain(recipe, "psize_Ni", dv={"psize_Ni": 1000})
     sgconstrain(recipe, "Ni")
     free_and_fit(recipe, 'all')
 
@@ -30,9 +33,16 @@ def test_fit_ni():
         save = gen_save_all(temp_folder, csv_db, fgr_db, cif_db)
         save(recipe)
 
-        print(pd.read_csv(csv_db).to_string())
-        print(pd.read_csv(fgr_db).to_string())
-        print(pd.read_csv(cif_db).to_string())
+        csv_df = pd.read_csv(csv_db)
+        fgr_df = pd.read_csv(fgr_db)
+        cif_df = pd.read_csv(cif_db)
+
+        print(csv_df.to_string())
+        print(fgr_df.to_string())
+        print(cif_df.to_string())
+
+        df = pd.merge(csv_df, fgr_df, how="left", on="recipe_id")
+        summarize(df, printout=True)
 
     return
 
